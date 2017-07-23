f0976884963c106d0a5c1b9ef5ca0f171471941b
ahmedtehseen <ahmedpisces7@gmail.com>
2017-07-23 15:59:09 +0500

    changed minLength to minlength in mongoose models

--- meetup-backend/src/modules/groups/model.js
+++ meetup-backend/src/modules/groups/model.js
@@ -5,12 +5,12 @@ const GroupSchema = new Schema({
     type: String,
     required: true,
     unique: true,
-    minLength: [5, 'Name must be 5 characters long'],
+    minlength: [5, 'Name must be 5 characters long'],
   },
   description: {
     type: String,
     required: true,
-    minLength: [10, 'Description must be 10 characters long'],
+    minlength: [10, 'Description must be 10 characters long'],
   },
   category: {
     type: String,
